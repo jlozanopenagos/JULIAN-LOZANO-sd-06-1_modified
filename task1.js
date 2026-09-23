3c4fa8fd11a759adbb694506a029ea0fbcb94f99
Julian Steven Lozano Penagos <lozano.julian05@gmail.com>
2026-09-23 15:45:01 -0500

    task 1 done

--- task1.js
+++ task1.js
@@ -5,7 +5,7 @@ function Mail(asunto, mensaje) {
 
 // ¡Escribe tu código debajo de esta línea!
 
-const nuevoCorreo = new Mail()
+const nuevoCorreo = new Mail("hola","mundo");
 
 // ¡Escribe tu código encima de esta línea!
 
