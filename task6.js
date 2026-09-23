e113ab552639206472c269759dd8b43804eabc54
Julian Steven Lozano Penagos <lozano.julian05@gmail.com>
2026-09-23 16:46:50 -0500

    task 6 & 7 done

--- task6.js
+++ task6.js
@@ -1,5 +1,40 @@
 // ¡Escribe tu código debajo de esta línea!
+function ShoppingList(items) {
+    this.items = items;
+}
 
+const cantidadProductos = Number(process.argv[3]);
+const productos = [];
+
+//initial position of the argument
+let posicion = 4;
+
+// node index.js 6 3 leche 2 huevos 12 pan 1
+for (let i = 0; i < cantidadProductos; i++) {
+    const nombre = process.argv[posicion];
+    const cantidad = Number(process.argv[posicion + 1]);
+
+    productos.push({
+        nombre: nombre,
+        cantidad: cantidad
+    });
+
+    posicion += 2;
+}
+
+const lista = new ShoppingList(productos);
+
+console.log(lista);
 
 
 // ¡Escribe tu código encima de esta línea!
+
+/*
+*  6. ¿Pueden crear un constructor para un objeto que describa una lista de compras? ¡Usen esta tarea para experimentar con constructores!
+    - ¿Qué tan larga debería ser la lista?
+        - No todos los usuarios querrán la misma cantidad de artículos.
+    - ¿Cómo manejarían múltiplos del mismo artículo, como 2 botellas de leche o una docena de huevos?
+    - ¿Cómo recolectarían estos datos del usuario?
+    - ¿Cómo almacenarían estos datos en un objeto?
+    - ¿Cómo se vería la función constructora para este objeto? 
+*/
--- task7.js
+++ task7.js
@@ -1,5 +1,43 @@
 // ¡Escribe tu código debajo de esta línea!
+function Car(marca, modelo, año, color, puertas, kilometraje, tipoMotor) {
+    this.marca = marca;
+    this.modelo = modelo;
+    this.año = año;
+    this.color = color;
+    this.puertas = puertas;
+    this.kilometraje = kilometraje;
+    this.tipoMotor = tipoMotor;
+}
 
+const marca = process.argv[3];
+const modelo = process.argv[4];
+const año = Number(process.argv[5]);
+const color = process.argv[6];
+const puertas = Number(process.argv[7]);
+const kilometraje = Number(process.argv[8]);
+const tipoMotor = process.argv[9];
 
+const auto = new Car(
+    marca,
+    modelo,
+    año,
+    color,
+    puertas,
+    kilometraje,
+    tipoMotor
+);
+
+console.log(auto);
 
 // ¡Escribe tu código encima de esta línea!
+
+/*
+*  7. ¿Pueden crear un constructor para un objeto que pueda describir un auto? ¡Usen esta tarea para experimentar con constructores!
+    - Piensen qué datos debería tener el objeto, cómo recolectarían esos datos del usuario y cómo los almacenarían.
+        - ¿La marca, modelo y año?
+        - ¿El color?
+        - ¿El número de puertas?
+        - ¿El kilometraje?
+        - ¿Si es de motor de combustión o eléctrico?
+        - ¿Algo más?
+*/
