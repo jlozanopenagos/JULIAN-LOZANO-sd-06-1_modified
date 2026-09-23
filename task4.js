885cf07d3199e98ca19a4681d5b3a706ded44b1f
Julian Steven Lozano Penagos <lozano.julian05@gmail.com>
2026-09-23 16:03:00 -0500

    task 4 done

--- task4.js
+++ task4.js
@@ -1,8 +1,22 @@
 // ¡Escribe tu código debajo de esta línea!
+/**
+ 4. Para esta tarea, necesitarán escribir su propio constructor.
+    - Creen un constructor llamado `Journey` que tome dos parámetros: `inicio` y `fin`.
+    - Creen un par de constantes llamadas `desde` y `hasta`, y asígnenles los valores de los argumentos de la línea de comandos.
+ */
 
+function Journey(inicio, fin) {
+  this.inicio = inicio
+  this.fin = fin
+  this.imprimirCorreo = function(){
+    return console.log(`${this.asunto}: ${this.mensaje}`);
+  }
 
+}
 
 // ¡Escribe tu código encima de esta línea!
+const desde = process.argv[3];
+const hasta = process.argv[4]; 
 
 const viaje = new Journey(desde, hasta)
 
